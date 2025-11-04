21e8bf0f877e2f5e1a1f9148528ecc4c7a933131
vivek <vivekrajput1924345@gmail.com>
2025-11-04 17:14:45 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -13,42 +13,22 @@ import Marquee from './components/Marquee'
 function App() {
   return (
     <>
-
       <Header />
       <div className="min-h-screen flex flex-col bg-white dark:bg-black  text-black dark:text-white transition-all duration-300 font-jetbrains-mono">
-
-        {/* Main Content Area */}
         <div className="flex-1 flex flex-col overflow-hidden">
-
-          {/* Scrollable Page Content */}
           <div className="relative flex-1 overflow-y-auto px-4 md:px-6   scrollbar-hide">
-
-
-           
             <Home />
-
-           <div
-  className="px-5 py-4 text-gray-900 dark:text-[#EFEFE6] transition-colors duration-300"
->
-  <Marquee speed={90} gap={60}>
-    <span className="text-lg sm:text-xl font-semibold whitespace-nowrap">
-      Laravel Developer ⚡ Full-Stack Creator ⚡ Clean & Scalable Code ⚡ Tailwind Enthusiast ⚡ Livewire Expert ⚡ Passion for Design & Motion ⚡ GSAP Animation Lover ⚡ Problem Solver ⚡ Creative Thinker ⚡
-    </span>
-  </Marquee>
-</div>
-
-
+            <div className="px-5 py-4 text-gray-900 dark:text-[#EFEFE6] transition-colors duration-300">
+              <Marquee speed={90} gap={60}>
+                <span className="text-lg sm:text-xl font-semibold whitespace-nowrap">
+                  Laravel Developer ⚡ Full-Stack Creator ⚡ Clean & Scalable Code ⚡ Tailwind Enthusiast ⚡ Livewire Expert ⚡ Passion for Design & Motion ⚡ GSAP Animation Lover ⚡ Problem Solver ⚡ Creative Thinker ⚡
+                </span>
+              </Marquee>
+            </div>
             <About />
             <Skills />
             <Project />
             <Contact />
-
-            {/* Contact Section */}
-            {/* <section id="contact" className="w-full">
-            <div className="min-h-[80vh] w-full mx-auto flex justify-center items-center border border-dashed border-gray-600 rounded-lg shadow p-6 md:p-10 dark:border-gray-300 transition-all">
-              <h2 className="text-4xl font-bold text-center">Contact</h2>
-            </div>
-          </section> */}
           </div>
         </div>
       </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -82,67 +82,44 @@ export default function Home() {
   };
 
   return (
-    <section
-      ref={sectionRef}
-      id="home"
-      className="relative h-screen overflow-hidden text-[#EFEFE6]"
-    >
-      <div
-        ref={iconsRef}
-        className="hidden md:flex absolute left-4 bottom-1/4 flex-col items-center text-gray-400 z-20 space-y-4"
-      >
+    <section ref={sectionRef} id="home" className="relative h-screen overflow-hidden text-[#EFEFE6]">
+      <div ref={iconsRef} className="hidden md:flex absolute left-4 bottom-1/4 flex-col items-center text-gray-400 z-20 space-y-4">
         <a href="#"><FaFacebookF className="hover:text-orange-500 transition" /></a>
         <a href="#"><FaTwitter className="hover:text-orange-500 transition" /></a>
         <a href="#"><FaLinkedinIn className="hover:text-orange-500 transition" /></a>
         <div className="h-12 w-px bg-gray-300 dark:bg-gray-600 opacity-50 mt-2"></div>
       </div>
 
-      {/* Scroll Indicator */}
+
       <div className="hidden md:flex absolute right-4 bottom-1/4 flex-col items-center z-20">
         <span className="text-sm tracking-widest text-gray-400 rotate-90">SCROLL</span>
         <div className="w-px h-12 bg-gray-300 dark:bg-gray-600 opacity-50 mt-6"></div>
       </div>
-      {/* Side Email */}
+
       <div className="absolute top-1/2 -left-20 hidden sm:flex rotate-90 bg-[#EFEFE6] text-black px-12 py-6">
         vivekrajput1924345@gmail.com
       </div>
 
-      {/* Background */}
       <div className="absolute inset-0 z-0"></div>
 
-      {/* Content */}
       <div className="relative z-10 h-screen w-full mx-auto flex flex-col lg:flex-row justify-center items-center gap-10 lg:gap-16 p-6 md:p-8 ">
-        {/* Social Icons */}
-
         <div ref={imagePin} className="absolute h-screen w-1/2 opacity-10 top-0  ">
           <img className="h-full w-full object-cover" src="/person1.png" alt="" />
         </div>
 
-        {/* Center Content */}
         <div className="relative  w-full h-screen justify-between flex flex-col py-10 max-w-5xl text-center">
-          <h1
-            ref={titleRef}
-            className="font-[font1] text-black dark:text-white text-[7vw] leading-[6vw] uppercase"
-          >
+          <h1 ref={titleRef} className="font-[font1] text-black dark:text-white text-[7vw] leading-[6vw] uppercase">
             Turning Bold Ideas into <br className="hidden sm:block" />  Web Products
           </h1>
-
-          {/* Buttons */}
           <div className="flex flex-col sm:flex-row justify-center gap-4 mt-6">
-            <button
-              ref={contactBtnRef}
-              className="font-[font1] px-8  pt-2 dark:text-white text-[4vw] leading-[6vw] uppercase border border-black dark:border-white  text-black rounded-full hover:border-orange-600  dark:hover:border-orange-600 dark:hover:text-orange-600 hover:text-orange-600 transition duration-300"
-            >
+            <button ref={contactBtnRef} className="font-[font1] px-8  pt-2 dark:text-white text-[4vw] leading-[6vw] uppercase border border-black dark:border-white  text-black rounded-full hover:border-orange-600  dark:hover:border-orange-600 dark:hover:text-orange-600 hover:text-orange-600 transition duration-300">
               Projects
             </button>
-            <button
-              ref={resumeBtnRef}
-              onClick={handleResumeClick}
-              className="font-[font1] px-8  pt-2 dark:text-white text-[4vw] leading-[6vw] uppercase border border-black dark:border-white  text-black rounded-full hover:border-orange-600 dark:hover:border-orange-600 dark:hover:text-orange-600 hover:text-orange-600 transition duration-300"
-            >
+            <button ref={resumeBtnRef} onClick={handleResumeClick} className="font-[font1] px-8  pt-2 dark:text-white text-[4vw] leading-[6vw] uppercase border border-black dark:border-white  text-black rounded-full hover:border-orange-600 dark:hover:border-orange-600 dark:hover:text-orange-600 hover:text-orange-600 transition duration-300">
               RESUME
             </button>
           </div>
+
         </div>
       </div>
     </section>
